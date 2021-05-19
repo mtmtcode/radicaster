252edb2712a10a4afbbb0ee7a4a301b85a45c562
l3msh0 <L3msh0@gmail.com>
2021-05-20 06:48:04 +0900

    :+1: Add GenFeed::S3#find_definition

--- gen_feed/lib/gen-feed/s3.rb
+++ gen_feed/lib/gen-feed/s3.rb
@@ -1,10 +1,25 @@
+require "yaml"
+
 module Radicaster
   module GenFeed
     class S3
       FEED_FILENAME = "index.rss"
+      DEFINITION_FILENAME = "radicaster.yaml"
 
       def initialize(client)
         @client = client
+      end
+
+      def find_definition(bucket, prefix)
+        key = "#{prefix}/#{DEFINITION_FILENAME}"
+        resp = client.get_object(bucket: bucket, key: key)
+        def_hash = YAML.load(resp.body.read)
+        Definition.new(
+          title: def_hash["title"],
+          author: def_hash["author"],
+          summary: def_hash["summary"],
+          image: def_hash["image"],
+        )
       end
 
       def save(bucket, prefix, feed_body)
--- gen_feed/spec/s3_spec.rb
+++ gen_feed/spec/s3_spec.rb
@@ -1,11 +1,38 @@
 module Radicaster::GenFeed
   describe S3 do
+    let(:client) { double("client") }
+    let(:bucket) { "dummy-bucket" }
+    let(:prefix) { "dummy-prefix" }
+    subject(:s3) { S3.new(client) }
+
+    describe "#find_definition" do
+      let(:def_body) do
+        <<-EOS
+        title: dummy-title
+        author: dummy-author
+        summary: dummy-summary
+        image: http://foo.test/bar.png
+        EOS
+      end
+
+      it "finds a defintion file according to passed prefix and returns a Definition object" do
+        key = "dummy-prefix/radicaster.yaml"
+        resp = double("response", body: StringIO.new(def_body))
+        expect(client).to receive(:get_object)
+                            .with(bucket: bucket, key: key)
+                            .and_return(resp)
+
+        def_ = s3.find_definition(bucket, prefix)
+
+        expect(def_.title).to eq("dummy-title")
+        expect(def_.author).to eq("dummy-author")
+        expect(def_.summary).to eq("dummy-summary")
+        expect(def_.image).to eq("http://foo.test/bar.png")
+      end
+    end
+
     describe "#save" do
-      let(:client) { double("client") }
-      let(:bucket) { "dummy-bucket" }
-      let(:prefix) { "dummy-prefix" }
       let(:feed_body) { "feed_body" }
-      subject(:s3) { S3.new(client) }
 
       it "uploads passed body with proper key" do
         key = "dummy-prefix/index.rss"
