7693ddfc665863cef8b740e537b0a055e6dc42ef
l3msh0 <L3msh0@gmail.com>
2021-05-20 06:11:26 +0900

    :recycle: cosmetic

--- gen_feed/spec/s3_spec.rb
+++ gen_feed/spec/s3_spec.rb
@@ -7,9 +7,9 @@ module Radicaster::GenFeed
       let(:feed_body) { "feed_body" }
       subject(:s3) { S3.new(client) }
 
-      it "upload passed body with proper key" do
+      it "uploads passed body with proper key" do
         key = "dummy-prefix/index.rss"
-        expect(client).to receive(:put_object).with(bucket: bucket, key: key, body:feed_body)
+        expect(client).to receive(:put_object).with(bucket: bucket, key: key, body: feed_body)
         s3.save(bucket, prefix, feed_body)
       end
     end
--- rec_radiko/spec/s3_spec.rb
+++ rec_radiko/spec/s3_spec.rb
@@ -7,7 +7,7 @@ module Radicaster::RecRadiko
       let(:body) { StringIO.new("dummy\nstream") }
 
       subject(:s3) { S3.new(client) }
-      it "upload passed IO object to s3" do
+      it "uploads passed IO object to s3" do
         expect(client).to receive(:put_object).with(bucket: bucket, key: key, body: body)
         s3.save(bucket, key, body)
       end
