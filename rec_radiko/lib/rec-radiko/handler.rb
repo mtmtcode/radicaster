4a48fa0532485685560ead0b80d4ffc6a4b09eb2
l3msh0 <L3msh0@gmail.com>
2021-05-28 18:29:39 +0900

    :shirt: Remove unnecessary comment

--- rec_radiko/lib/rec-radiko/handler.rb
+++ rec_radiko/lib/rec-radiko/handler.rb
@@ -66,10 +66,6 @@ module Radicaster
         local_path = recorder.rec(cmd.area, cmd.station, cmd.starts)
         logger.info("Finished recording")
 
-        # TODO 抽象度上げる
-        # - recしたらEpisode(?)が返ってくる
-        # - storageにはEpisodeを渡す
-
         logger.info("Saving the episode to the storage")
         File.open(local_path, "rb") do |f|
           storage.save(cmd.id, cmd.starts[0], f)
