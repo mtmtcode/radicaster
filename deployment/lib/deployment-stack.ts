46d4cded47e6ffe1455afbbab94b8761c31c0287
l3msh0 <L3msh0@gmail.com>
2021-05-31 07:04:44 +0900

    :construction: wip

--- deployment/lib/deployment-stack.ts
+++ deployment/lib/deployment-stack.ts
@@ -1,5 +1,8 @@
-import { BlockPublicAccess, Bucket, BucketProps } from '@aws-cdk/aws-s3';
+import { Bucket, BucketProps } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
+import { Code, DockerImageCode, DockerImageFunction, Function } from '@aws-cdk/aws-lambda';
+import { DockerImage, Duration } from '@aws-cdk/core';
+import { ManagedPolicy, Policy, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
 
 export class DeploymentStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -20,6 +23,29 @@ export class DeploymentStack extends cdk.Stack {
     // rec-radiko
     // - Lambda作る
     // - ECRのリポジトリ作る？
+    const lambdaExecutionPolicy = ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole");
+    new Role(this, 'role-rec-radiko', {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [lambdaExecutionPolicy],
+    });
+
+    const funcRecRadiko = new DockerImageFunction(this, 'func-rec-radiko', {
+      code: DockerImageCode.fromImageAsset(
+        "../rec_radiko"
+      ),
+      timeout: Duration.minutes(10),
+      memorySize: 768,
+    });
+    funcRecRadiko.grantInvoke(new ServicePrincipal("events.amazonaws.com"));
+
+    // Image
+    // new Function(this, 'lambda-rec-radiko', {
+    //   functionName: 'rec-radiko',
+
+    //   environment: {
+    //     // TODO
+    //   },
+    // })
 
     // gen-feed
     // - Lambda作る
