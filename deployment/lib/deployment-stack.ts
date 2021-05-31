09d5d39c9d5f593b14d718347d8544dc718622f0
l3msh0 <L3msh0@gmail.com>
2021-05-31 19:03:09 +0900

    :construction: Deploy rec-radiko

--- deployment/lib/deployment-stack.ts
+++ deployment/lib/deployment-stack.ts
@@ -2,7 +2,7 @@ import { Bucket, BucketProps } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 import { Code, DockerImageCode, DockerImageFunction, Function } from '@aws-cdk/aws-lambda';
 import { DockerImage, Duration } from '@aws-cdk/core';
-import { ManagedPolicy, Policy, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
+import { Effect, ManagedPolicy, Policy, PolicyDocument, PolicyStatement, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
 
 export class DeploymentStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +11,9 @@ export class DeploymentStack extends cdk.Stack {
     // The code that defines your stack goes here
     const bucketName = this.getEnv('RADICASTER_S3_BUCKET');
     const bucketURL = this.getEnv('RADICASTER_BUCKET_URL');
+    // TODO メールとパスワードは任意なのでエラー出ないように
+    const radikoMail = this.getEnv('RADICASTER_RADIKO_MAIL');
+    const radikoPassword = this.getEnv('RADICASTER_RADIKO_PASSWORD');
 
     // S3
     const bucketProps: BucketProps = {
@@ -21,22 +24,24 @@ export class DeploymentStack extends cdk.Stack {
     const bucket = new Bucket(this, 'bucket', bucketProps);
 
     // rec-radiko
-    // - Lambda作る
-    // - ECRのリポジトリ作る？
-    const lambdaExecutionPolicy = ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole");
-    new Role(this, 'role-rec-radiko', {
-      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
-      managedPolicies: [lambdaExecutionPolicy],
-    });
-
     const funcRecRadiko = new DockerImageFunction(this, 'func-rec-radiko', {
       code: DockerImageCode.fromImageAsset(
         "../rec_radiko"
       ),
       timeout: Duration.minutes(10),
       memorySize: 768,
+      environment: {
+        // TODO Secrets Managerとか使う
+        "RADICASTER_RADIKO_MAIL": radikoMail,
+        "RADICASTER_RADIKO_PASSWORD": radikoPassword,
+        "RADICASTER_S3_BUCKET": bucketName,
+      }
     });
     funcRecRadiko.grantInvoke(new ServicePrincipal("events.amazonaws.com"));
+    if (!funcRecRadiko.role) {
+      throw new Error("funcRecRadiko.role is undefined");
+    }
+    bucket.grantPut(funcRecRadiko.role);
 
     // Image
     // new Function(this, 'lambda-rec-radiko', {
