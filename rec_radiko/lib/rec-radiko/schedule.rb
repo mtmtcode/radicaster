076b624df60edb482f02c3cd4d8dfaa0f20f0192
l3msh0 <L3msh0@gmail.com>
2021-06-15 23:48:39 +0900

    :+1: Add Schedule#latest

---
+++ rec_radiko/lib/rec-radiko/schedule.rb
@@ -0,0 +1,19 @@
+module Radicaster
+  module RecRadiko
+    class Schedule
+      attr_reader :items
+
+      def initialize(items)
+        @items = items
+      end
+
+      def latest(now)
+        # 各itemをlatestを取得して、最初の要素が直近のものを返す
+        items
+          .map { |x| x.map { |y| y.latest(now) } }
+          .sort { |a, b| a[0] <=> b[0] }
+          .last
+      end
+    end
+  end
+end
---
+++ rec_radiko/spec/schedule_spec.rb
@@ -0,0 +1,24 @@
+module Radicaster::RecRadiko
+  describe Schedule do
+    describe "#latest" do
+      let(:now) { Time.new(2021, 6, 15, 0, 0, 0, "+09:00") }  # tuesday
+
+      it "returns latest schedule item" do
+        items = [
+          [ScheduleTime.new("Mon", 8, 30, 0), ScheduleTime.new("Mon", 10, 0, 0)],
+          [ScheduleTime.new("Tue", 8, 30, 0), ScheduleTime.new("Tue", 10, 0, 0)],
+          [ScheduleTime.new("Wed", 8, 30, 0), ScheduleTime.new("Wed", 10, 0, 0)],
+          [ScheduleTime.new("Thu", 8, 30, 0), ScheduleTime.new("Thu", 10, 0, 0)],
+          [ScheduleTime.new("Fri", 8, 30, 0), ScheduleTime.new("Fri", 10, 0, 0)],
+        ]
+        schedule = Schedule.new(items)
+
+        expected = [
+          Time.new(2021, 6, 14, 8, 30, 0, "+09:00"),
+          Time.new(2021, 6, 14, 10, 0, 0, "+09:00"),
+        ]
+        expect(schedule.latest(now)).to eq(expected)
+      end
+    end
+  end
+end
