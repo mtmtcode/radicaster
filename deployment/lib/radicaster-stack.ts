fbc364ebd531e3f0fd743ab26a74e2aaf49cabb5
mtmtcode <l3msh0@gmail.com>
2026-02-20 00:18:20 +0900

    公式のNodeランタイムに乗り換え

--- deployment/lib/radicaster-stack.ts
+++ deployment/lib/radicaster-stack.ts
@@ -4,6 +4,7 @@ import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { CanonicalUserPrincipal, Effect, PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Code, DockerImageCode, DockerImageFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Bucket, EventType } from 'aws-cdk-lib/aws-s3';
 import { SnsDestination } from 'aws-cdk-lib/aws-s3-notifications';
 import { Topic } from 'aws-cdk-lib/aws-sns';
@@ -97,16 +98,21 @@ export class RadicasterStack extends cdk.Stack {
   private setUpFuncGenFeed(bucket: Bucket, dist: Distribution, params: Params, topic: Topic) {
     const authPrefix = `${params.basicAuthUser}:${params.basicAuthPassword}@`
     const domainName = params.customDomain || dist.domainName;
-    const funcGenFeed = new DockerImageFunction(this, `func-gen-feed`, {
-      code: DockerImageCode.fromImageAsset(
-        "../gen_feed"
-      ),
-      functionName: `radicaster-gen-feed${params.suffix}`,
+    const funcGenFeed = new NodejsFunction(this, `func-gen-feed`, {
+      entry: path.join(__dirname, '../../gen_feed/src/handler.ts'),
+      handler: 'handler',
+      runtime: Runtime.NODEJS_20_X,
+      functionName: `radicaster-gen-feed-node${params.suffix}`,
       timeout: Duration.minutes(1),
       memorySize: 128,
       environment: {
         "RADICASTER_S3_BUCKET": params.bucketName,
         "RADICASTER_BUCKET_URL": `https://${authPrefix}${domainName}`,
+      },
+      bundling: {
+        forceDockerBundling: false,
+        sourceMap: true,
+        minify: true,
       }
     });
     funcGenFeed.grantInvoke(new ServicePrincipal("events.amazonaws.com"));
